c1bce81763c49e2a06c9980afb1d4f9d05803c3a
steveharg <steveh@fastmail.fm>
2018-08-02 17:20:25 +0100

    started work to use motif name lists instead of glycan IDs as leaf node labels - not complete

--- upgma_glycans.py
+++ upgma_glycans.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from Bio.Phylo.TreeConstruction import DistanceMatrix, DistanceTreeConstructor, DistanceCalculator
 from Bio import Phylo
+import json
 
 if __name__ == "__main__":
 
@@ -16,8 +17,54 @@ if __name__ == "__main__":
     column_names = pickle.load(open(data_dir + "/column_names.p", "rb"))
     print("...done")
 
+    # create a glycan id to motif ids dictionary
+    glycan_motifs = {}
+
+    print("opening motif_ids_and_labels.json...")
+    with open('motif_ids_and_labels.json') as f:
+        motif_ids_and_labels = json.load(f)
+    print("...done")
+
+    # motif_ids_and_labels_dict
+    motif_ids_and_labels_dict = {}
+    for result in motif_ids_and_labels["results"]["bindings"]:
+        motif_primary_id = result["MotifPrimaryId"]["value"]
+        motif_label = result["MotifLabel"]["value"]
+        motif_ids_and_labels_dict[motif_primary_id] = motif_label
+
+    json_file = 'glycans.json'
+    with open(json_file) as f:
+        results = json.load(f)
+
+    for result in results["results"]["bindings"]:
+        glycan_id = result["PrimaryId"]["value"]
+        if glycan_id not in glycan_motifs:
+            glycan_motifs[glycan_id] = []
+        if "MotifPrimaryId" in result:
+            motif_id = result["MotifPrimaryId"]["value"]
+            if motif_id not in glycan_motifs[glycan_id]:
+                glycan_motifs[glycan_id].append(motif_id)
+
+    # replace motif ids with motif names
+    for glycan_id in glycan_motifs:
+        motif_ids = sorted(glycan_motifs[glycan_id])
+        motif_names = []
+        for motif_id in motif_ids:
+            motif_names.append(motif_ids_and_labels_dict[motif_id])
+        glycan_motifs[glycan_id] = ', '.join(motif_names)
+
     reactions_heatmap_items = heatmaps[0]
     glycan_distance_cols = column_names[0]
+
+    # replace glycan_ids in glycan_distance_cols with motif name lists
+    glycan_distance_cols_temp = []
+    for glycan_id in glycan_distance_cols:
+        print("appending:", glycan_motifs[glycan_id])
+        glycan_distance_cols_temp.append(glycan_motifs[glycan_id])
+        if glycan_id == "G00001NT":
+            print('no motifs')
+
+    glycan_distance_cols = glycan_distance_cols_temp
 
     reaction_distance_df = pd.DataFrame.from_dict(reactions_heatmap_items,
                                                   orient='index', columns=glycan_distance_cols)
