d9e4f3097b2aef02c63c71f3b2af02e249fb2a65
steveharg <steveh@fastmail.fm>
2018-08-03 13:00:53 +0100

    can now choose whether or not to include zero-motif glycans

--- wurcs_to_reaction_distances_file.py
+++ wurcs_to_reaction_distances_file.py
@@ -9,18 +9,25 @@ if __name__ == "__main__":
 
     num_args = len(sys.argv)
 
-    num_datafile_lines = None
+    include_zero_motif_glycans = False
+    num_datafile_lines = None # None means use all datafile lines
 
     if num_args > 1:
-        num_datafile_lines = int(sys.argv[1])
+        include_zero_motif_glycans = bool(sys.argv[1])
+    if num_args > 2:
+        num_datafile_lines = int(sys.argv[2])
 
     reactions_bag = parse_glytoucan_results('glycans.json', num_datafile_lines)
-    reactions_bag = filter_by_common_motifs(reactions_bag)
+    reactions_bag = filter_by_common_motifs(reactions_bag, include_zero_motif_glycans)
     heatmaps, column_names = calc_jaccard_distances(reactions_bag)
 
     reaction_distances = [reactions_bag, heatmaps, column_names]
 
-    data_dir = "data_top10_motifs"
+    if include_zero_motif_glycans:
+        data_dir = "data_top10_and_zero_motifs"
+    else:
+        data_dir = "data_top10_motifs"
+
     # data_dir = "mini_data_top10_motifs"
 
     pickle.dump(reactions_bag, open(data_dir + "/reactions_bag.p", "wb"))
