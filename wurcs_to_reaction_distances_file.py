445822207b36b3566595d6baa47f5bb173390cba
steveharg <steveh@fastmail.fm>
2018-08-24 15:58:39 +0100

    uses config file and removed commented out code

--- wurcs_to_reaction_distances_file.py
+++ wurcs_to_reaction_distances_file.py
@@ -1,42 +1,39 @@
-import sys
 import pickle
-import json
 from parse_glytoucan_results import parse_glytoucan_results
 from calc_jaccard_distances import calc_jaccard_distances
 from filter_by_common_motifs import filter_by_common_motifs
+import configparser
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read('config.ini')
 
 if __name__ == "__main__":
 
-    num_args = len(sys.argv)
-
-    include_zero_motif_glycans = False
-    num_datafile_lines = None # None means use all datafile lines
-    use_reaction_quantities = True
-
-    if num_args > 1:
-        include_zero_motif_glycans = 'True' == sys.argv[1]
-    if num_args > 2:
-        num_datafile_lines = int(sys.argv[2])
-
-    reactions_bag = parse_glytoucan_results('glycans.json', num_datafile_lines, use_reaction_quantities=use_reaction_quantities)
-    reactions_bag = filter_by_common_motifs(reactions_bag, include_zero_motif_glycans)
-    heatmaps, column_names = calc_jaccard_distances(reactions_bag, use_reaction_quantities=use_reaction_quantities)
-
-    reaction_distances = [reactions_bag, heatmaps, column_names]
+    include_zero_motif_glycans = config['DEFAULT'].getboolean('IncludeZeroMotifGlycans')
+    num_datafile_lines = config['DEFAULT'].getint('NumDatafileLines')  # None means use all datafile lines
+    use_reaction_quantities = True if config['DEFAULT']['ReactionCountMethod'] == 'List' else False
+    glycans_json_file = config['DEFAULT']['GlycansJsonFile']
+    reactions_collection_pickle_file = config['DEFAULT']['ReactionsCollectionPickleFile']
+    heatmaps_pickle_file = config['DEFAULT']['HeatmapsPickleFile']
+    col_names_pickle_file = config['DEFAULT']['ColNamesPickleFile']
 
     if include_zero_motif_glycans:
         if use_reaction_quantities:
-            data_dir = "data_top10_with_reac_quants_and_zero_motifs"
+            data_dir = config['DEFAULT']['QuantifiedReactionsZeroMotifsDataDir']
         else:
-            data_dir = "data_top10_and_zero_motifs"
+            data_dir = config['DEFAULT']['SetOfReactionsZeroMotifsDataDir']
     else:
         if use_reaction_quantities:
-            data_dir = "data_top10_with_reac_quants_motifs"
+            data_dir = config['DEFAULT']['QuantifiedReactionsDataDir']
         else:
-            data_dir = "data_top10_motifs"
+            data_dir = config['DEFAULT']['SetOfReactionsDataDir']
 
-    # data_dir = "mini_data_top10_motifs"
+    reactions_collection = parse_glytoucan_results(glycans_json_file, num_datafile_lines, use_reaction_quantities)
+    reactions_collection = filter_by_common_motifs(reactions_collection, include_zero_motif_glycans)
+    heatmaps, column_names = calc_jaccard_distances(reactions_collection, use_reaction_quantities)
 
-    pickle.dump(reactions_bag, open(data_dir + "/reactions_bag.p", "wb"))
-    pickle.dump(heatmaps, open(data_dir + "/heatmaps.p", "wb"))
-    pickle.dump(column_names, open(data_dir + "/column_names.p", "wb"))
+    reaction_distances = [reactions_collection, heatmaps, column_names]
+
+    pickle.dump(reactions_collection, open(data_dir + "/" + reactions_collection_pickle_file, "wb"))
+    pickle.dump(heatmaps, open(data_dir + "/" + heatmaps_pickle_file, "wb"))
+    pickle.dump(column_names, open(data_dir + "/" + col_names_pickle_file, "wb"))
