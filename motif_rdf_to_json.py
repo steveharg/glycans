8c1f5a01ec89330534fc5cccf4d67307942fae9f
steveharg <steveh@fastmail.fm>
2018-08-24 15:57:17 +0100

    uses config file and removed commented out code

--- motif_rdf_to_json.py
+++ motif_rdf_to_json.py
@@ -1,27 +1,22 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
+import configparser
 
-sparql = SPARQLWrapper("https://ts.glytoucan.org/sparql")
-sparql.setQuery("""
-    PREFIX glycan: <http://purl.jp/bio/12/glyco/glycan#>
-    PREFIX glytoucan:  <http://www.glytoucan.org/glyco/owl/glytoucan#>
+config = configparser.ConfigParser(allow_no_value=True)
+config.read('config.ini')
 
-    SELECT DISTINCT ?Motif ?MotifPrimaryId ?MotifLabel
-    FROM <http://rdf.glytoucan.org/core>
-    FROM <http://rdf.glytoucan.org/sequence/wurcs>
-    FROM <http://rdf.glytoucan.org/motif>
-    WHERE {
-            ?Saccharide glycan:has_motif ?Motif .
-            ?Motif glytoucan:has_primary_id ?MotifPrimaryId.
-            ?Motif rdfs:label ?MotifLabel
-    }
-    ORDER BY ?MotifPrimaryId
-""")
-sparql.setReturnFormat(JSON)
-# sparql.setReturnFormat(N3)
-results = sparql.query().convert()
+if __name__ == "__main__":
 
-# write json results to file
-with open('motif_ids_and_labels.json', 'w') as f:
-    json.dump(results, f, ensure_ascii=False)
+    sparql_endpoint_url = config['DEFAULT']['SparqlEndpointUrl']
+    sparql_query = config['DEFAULT']['MotifIdAndLabelSparqlQuery']
+    motif_ids_and_labels_json_file = config['DEFAULT']['MotifIdsAndLabelsJsonFile']
+
+    sparql = SPARQLWrapper(sparql_endpoint_url)
+    sparql.setQuery(sparql_query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    # write json results to file
+    with open(motif_ids_and_labels_json_file, 'w') as f:
+        json.dump(results, f, ensure_ascii=False)
 
