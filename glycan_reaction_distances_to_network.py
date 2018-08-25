69c8826e5fdbd39c96f44df8d3a33df4f2d72ea8
steveharg <steveh@fastmail.fm>
2018-08-25 16:14:31 +0100

    completed implementation for reaction quantities method

--- glycan_reaction_distances_to_network.py
+++ glycan_reaction_distances_to_network.py
@@ -45,26 +45,39 @@ if __name__ == "__main__":
                                                    orient='index', columns=motif_distance_cols)
 
     if use_reaction_quantities:
+        # open the graph G previously created using the reaction set method
         print("opening G_" + str(threshold) + ".p from " + config['DEFAULT']['SetOfReactionsDataDir'] + "/...")
         G_set = pickle.load(open(config['DEFAULT']['SetOfReactionsDataDir'] + "/G_" + str(threshold) + ".p", "rb"))
         print("...done")
+        # count its number of edges
         num_edges_in_g_set = len(G_set.edges)
         print("num_edges_in_g_set:", num_edges_in_g_set)
-        # G_set_ordered_edges = sorted(G_set.edges(data=True), key=lambda x: x[2]['weight'], reverse=True)
-        # print("G_set_ordered_edges:", G_set_ordered_edges[0:200])
-        # sys.exit(0)
 
-        glycan_name_and_idx = dict(zip(range(0, len(glycan_distance_cols)), glycan_distance_cols))
+        # create a new graph from the full glycan similarity matrix created using the reaction quantities method,
+        # applying the similarity measure as edge weights
         G_list = nx.from_numpy_array(reaction_distance_df.values)
-        G_list2 = nx.relabel_nodes(G_list, glycan_name_and_idx)
-        g_list_edges = G_list2.edges(data=True)
-        # print("unordered g_list_edges:", g_list_edges)
-        # ordered_edges = sorted(g_list_edges, key=lambda edge: edge['weight'])
-        ordered_edges = sorted(g_list_edges, key=lambda x: x[2]['weight'], reverse=True)
-        print("ordered_edges:", ordered_edges)
-        truncated_edges_list = ordered_edges[0:num_edges_in_g_set]
-        print("truncated_edges_list:", truncated_edges_list)
-        sys.exit(0)
+
+        # the nodes of this new graph only have the indices of the similarity matrix as labels, so relabel using glycan
+        # IDs
+        glycan_name_and_idx = dict(zip(range(0, len(glycan_distance_cols)), glycan_distance_cols))
+        G_list_relabelled = nx.relabel_nodes(G_list, glycan_name_and_idx)
+
+        # sort the edges by weight
+        G_list_edges = G_list_relabelled.edges(data=True)
+        ordered_edges = sorted(G_list_edges, key=lambda x: x[2]['weight'], reverse=True)
+
+        # create a new graph using only the same number of (highest weight) edges present in the original 'reaction set
+        # method' graph
+        G = nx.Graph()
+        G.add_edges_from(ordered_edges[0:num_edges_in_g_set])
+
+        # save the new graph. the threshold value in the filename refers to the threshold used to produce
+        # the corresponding graph created using the reaction set method, from which we read the number of edges
+        # to be used in this graph
+        print("Pickling G_" + str(threshold) + ".p...")
+        pickle.dump(G, open(data_dir + "/G_" + str(threshold) + ".p", "wb"))
+        print("...done")
+
     else:
         # Apply threshold
         thresholded_reactions = []
@@ -81,7 +94,6 @@ if __name__ == "__main__":
         print("...done")
 
         cols = [x[0] for x in thresholded_reactions]
-        # print("cols:", cols)
         thresholded_reactions_df = pd.DataFrame.from_items(thresholded_reactions,
                                                            orient='index', columns=cols)
 
@@ -90,6 +102,7 @@ if __name__ == "__main__":
         print("...done")
         print("Pickling G_" + str(threshold) + "...")
         pickle.dump(G, open(data_dir + "/G_" + str(threshold) + ".p", "wb"))
+        print("...done")
 
     print("creating graph layout...")
     # pos = nx.spring_layout(G)
